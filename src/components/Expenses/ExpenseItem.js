682523e1ad4ac6e0de7cda2ab3f5eb5e65e42645
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2022-07-14 16:55:24 -0300

    using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import './ExpenseItem.css';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+	//hook to use state, to be able to turn it into another value and refresh the page
+	const [title, setTitle] = useState(props.title);
+	//the first element is the current state value, and the second, is a FUNCTION for update that
+
+	// console.log('ExpenseItem evaluated by React');
+	const clickHandler = () => {
+		setTitle('updated!');
+		console.log(title);
+	};
+
 	return (
 		//I have a Card where the props of it is className='expense-item'
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
-}
+};
 
 export default ExpenseItem;
