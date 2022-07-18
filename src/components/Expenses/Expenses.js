ff5c1c337d89b857c24da87a5465ab03da6026b6
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2022-07-18 16:53:56 -0300

    Creating ExpensesFilter and Using It in Expenses.js file, it is parent for now

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,50 @@
+import React, { useState } from 'react';
+
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2021');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
-		<Card className="expenses">
-			<ExpenseItem
-				title={props.data[0].title}
-				amount={props.data[0].amount}
-				date={props.data[0].date}
-			/>
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
 
-			<ExpenseItem
-				title={props.data[1].title}
-				amount={props.data[1].amount}
-				date={props.data[1].date}
-			/>
+				<ExpenseItem
+					title={props.data[0].title}
+					amount={props.data[0].amount}
+					date={props.data[0].date}
+				/>
 
-			<ExpenseItem
-				title={props.data[2].title}
-				amount={props.data[2].amount}
-				date={props.data[2].date}
-			/>
+				<ExpenseItem
+					title={props.data[1].title}
+					amount={props.data[1].amount}
+					date={props.data[1].date}
+				/>
 
-			<ExpenseItem
-				title={props.data[3].title}
-				amount={props.data[3].amount}
-				date={props.data[3].date}
-			/>
-		</Card>
+				<ExpenseItem
+					title={props.data[2].title}
+					amount={props.data[2].amount}
+					date={props.data[2].date}
+				/>
+
+				<ExpenseItem
+					title={props.data[3].title}
+					amount={props.data[3].amount}
+					date={props.data[3].date}
+				/>
+			</Card>
+		</div>
 
 		// <div className="expenses">
 		// 	{props.data.map((e) => {
@@ -42,6 +58,6 @@ const Expenses = (props) => {
 		// 	})}
 		// </div>
 	);
-}
+};
 
 export default Expenses;
