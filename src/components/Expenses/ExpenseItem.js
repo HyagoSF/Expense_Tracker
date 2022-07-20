cf892e0413518cc299d91ce1ee6939e267ccaa2f
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2022-07-20 16:06:50 -0300

    Changes before start make my Chart

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,12 +9,6 @@ const ExpenseItem = (props) => {
 	const [title, setTitle] = useState(props.title);
 	//the first element is the current state value, and the second, is a FUNCTION for update that
 
-	// console.log('ExpenseItem evaluated by React');
-	const clickHandler = () => {
-		setTitle('updated!');
-		console.log(title);
-	};
-
 	return (
 		//I have a Card where the props of it is className='expense-item'
 		<li>
@@ -25,7 +19,6 @@ const ExpenseItem = (props) => {
 					<h2>{title}</h2>
 					<div className="expense-item__price">${props.amount}</div>
 				</div>
-				<button onClick={clickHandler}>Change Title</button>
 			</Card>
 		</li>
 	);
