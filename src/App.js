e2db2eabf4fb9c6351b5720ccc41215ef5c76eb5
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2022-10-25 11:48:11 -0400

    started file

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
-import { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import React, { useState } from 'react';
 
-const DUMMY_EXPENSES = [
-	{
-		id: 'e1',
-		title: 'Toilet Paper',
-		amount: 94.12,
-		date: new Date(2020, 7, 14),
-	},
-	{
-		id: 'e2',
-		title: 'New MacBook',
-		amount: 799.49,
-		date: new Date(2021, 2, 12),
-	},
-	{
-		id: 'e3',
-		title: 'Car Insurance',
-		amount: 294.67,
-		date: new Date(2021, 2, 28),
-	},
-	{
-		id: 'e4',
-		title: 'New Desk (Wooden)',
-		amount: 450,
-		date: new Date(2021, 5, 12),
-	},
-];
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-const App = () => {
-	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-	const addExpenseHandler = (expense) => {
-		setExpenses((prevExpenses) => {
-			return [expense, ...prevExpenses];
-		});
-	};
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
 
-	return (
-		<div>
-			<NewExpense onAddExpense={addExpenseHandler} />
-			<Expenses items={expenses} />
-		</div>
-	);
-};
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
 
 export default App;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,9 @@
-import './Card.css';
+import React from 'react';
+
+import classes from './Card.module.css';
 
 const Card = (props) => {
-	const classes = `card ${props.className}`;
-	return <div className={classes}>{props.children}</div>;
-	// <div className="card expense-item"></div>;
-	// <div className="card expenses"></div>;
-}
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
+};
 
 export default Card;
